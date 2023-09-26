345b60f529b0a757ef3c6c970b19d01afa078de4
Muhammad Sinan <61152623+sinanLab@users.noreply.github.com>
2023-09-26 22:35:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 from arithmetic_arranger import arithmetic_arranger
+# Example 1: Formatting arithmetic problems without answers
+problems = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
+arranged_problems = arithmetic_arranger(problems, show_answers=True)
+
+# Example 2: Formatting arithmetic problems with answers
+problems = ["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"]
+arranged_problems = arithmetic_arranger(problems, show_answers=True)
