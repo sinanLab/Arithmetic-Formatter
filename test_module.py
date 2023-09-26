75b54ad680e6404ecf2d3c8fd8f9c8f47332b94c
Muhammad Sinan <61152623+sinanLab@users.noreply.github.com>
2023-09-26 22:16:48 +0800

    Update test_module.py

--- test_module.py
+++ test_module.py
@@ -5,4 +5,3 @@ arranged_problems = arithmetic_arranger(problems, show_answers=True)
 # Example 2: Formatting arithmetic problems with answers
 problems = ["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"]
 arranged_problems = arithmetic_arranger(problems, show_answers=True)
-print(arranged_problems)
